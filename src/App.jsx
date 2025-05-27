2f7aeba0371215917bdbe360276bc27ee27efa7b
zinabu kalayu <zinabuk@anbesabank.com>
2025-05-27 10:40:00 +0300

    unused home component removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import ForeignExchange from "./ForeignExchange";
 import PrivacyPolicy from "./PrivacyPolicy"; 
-import logo from "./image/logo.jpeg";
-function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center w-full py-24">
-      <img src={logo} alt="Logo" className="h-10 md:h-24" />
-      <h2 className="text-center text-3xl mt-10 font-bold">Lion International Bank</h2>
-      <p>Key to Success!</p>
-    </div>
-  );
-}
+
 export default function App() {
   return (
     <BrowserRouter>
